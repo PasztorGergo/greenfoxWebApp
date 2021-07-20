6c76f9278581026726a7b2a1e71a13df3c919374
Pásztor Gergő <pasztorg05@gmail.com>
2021-07-20 18:01:29 +0200

    jQuery import

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const tree = $('.tree');
+const cloud = $('.cloud');
+const sun = $('.sun');
+const nebula = $('.nebula');
+const eye = $('.eye');
+const girl = $('.girl');
+const something = $('.something');
+const ocean = $('.ocean');
+
+const photo = $('.primary img');
+const heading = $('#desc-heading');
+
