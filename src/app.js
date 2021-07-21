4b3f2cee90c6f51b765f91cc211c82b44d540a53
Pásztor Gergő <pasztorg05@gmail.com>
2021-07-21 17:42:40 +0200

    photo slider

--- src/app.js
+++ src/app.js
@@ -6,7 +6,24 @@ const eye = $('.eye');
 const girl = $('.girl');
 const something = $('.something');
 const ocean = $('.ocean');
+const allPhoto = [tree,cloud,sun,nebula,eye,girl,something,ocean];
 
+const primary = $('.primary');
 const photo = $('.primary img');
 const heading = $('#desc-heading');
 
+var pictureIndex =  0;
+
+$('.dec').click(()=>{
+    if(pictureIndex === 0)
+        pictureIndex = allPhoto.length;
+    pictureIndex--;
+    photo.attr('src',allPhoto[pictureIndex].css('background-image'));
+    alert(allPhoto[pictureIndex].css('background-image'));
+});
+$('.inc').click(()=>{
+    if(pictureIndex === allPhoto.length)
+        pictureIndex = 0;
+    pictureIndex++;
+    photo.attr('src',allPhoto[pictureIndex].css('background-image'));
+});
