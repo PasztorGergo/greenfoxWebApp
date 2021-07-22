6badc902712482f9cd2f943df73f96a84bad1599
Pásztor Gergő <pasztorg05@gmail.com>
2021-07-22 08:49:35 +0200

    final touches

--- src/app.js
+++ src/app.js
@@ -11,18 +11,30 @@ const allPhoto = [tree,cloud,sun,nebula,eye,girl,something,ocean];
 const primary = $('.primary');
 const photo = $('.primary img');
 const heading = $('#desc-heading');
-
+const photoReplace = ()=>{
+    $('.gallery-photo').removeClass('selected');
+    photo.attr('src',`images/${allPhoto[pictureIndex].attr('class').split(' ')[0]}.jpg`);
+    heading.text(allPhoto[pictureIndex].attr('data-title'));
+    allPhoto[pictureIndex].addClass('selected');
+}
 var pictureIndex =  0;
 
 $('.dec').click(()=>{
     if(pictureIndex === 0)
         pictureIndex = allPhoto.length;
     pictureIndex--;
-    photo.attr('src',`images/${allPhoto[pictureIndex].attr('class').split(' ')[0]}.jpg`);
+    photoReplace();
 });
 $('.inc').click(()=>{
     pictureIndex++;
     if(pictureIndex === allPhoto.length)
         pictureIndex = 0;
-    photo.attr('src',`images/${allPhoto[pictureIndex].attr('class').split(' ')[0]}.jpg`);
+    photoReplace();
+});
+$('.gallery').children().click((e)=>{
+   const element = e.target;
+   $('.gallery-photo').removeClass('selected');
+   element.classList.add('selected'); 
+   pictureIndex = allPhoto.findIndex(x => x[0].classList.item(0) == element.classList.item(0));
+   photoReplace()
 });
